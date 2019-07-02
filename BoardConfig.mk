454aefffeec916d442f6a0a6005ac6d84fe36624
HarshOne <harshbambhaniya111@gmail.com>
2019-07-02 16:28:17 +0530

    my first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#               2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# Inherit from common melsuhl-common
+-include device/htc/melsuhl-common/BoardConfigCommon.mk
+
+# Model Ids
+# 0P6B90000 - International
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := htc_melsuhl,melsuhl
+
+# Kernel
+TARGET_KERNEL_CONFIG := melsuhl_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/melsuhl/bluetooth
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/htc/melsuhl/ril
+TARGET_NEEDS_GCC_LIBC := true
+TARGET_NEEDS_GCC_LIBRIL := true
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
+
+# Vendor Init
+TARGET_INIT_VENDOR_LIB := libinit_melsuhl
+TARGET_RECOVERY_DEVICE_MODULES := libinit_melsuhl
+
+# Inherit from the proprietary version
+-include vendor/htc/melsuhl/BoardConfigVendor.mk
---
+++ init/init_melsuhl.cpp
@@ -0,0 +1,109 @@
+/*
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+#include "log.h"
+#include "util.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void common_properties()
+{
+    property_set("rild.libargs", "-d /dev/smd0");
+    property_set("ro.ril.hsdpa.category", "24");
+    property_set("ro.ril.hsxpa", "4");
+    property_set("ro.ril.disable.cpc", "1");
+}
+
+void cdma_properties(char const default_cdma_sub[], char const default_network[])
+{
+    property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
+    property_set("ro.telephony.default_network", default_network);
+    property_set("telephony.lteOnCdmaDevice", "1");
+    property_set("ro.ril.enable.sdr", "0");
+    property_set("persist.radio.snapshot_enabled", "1");
+    property_set("persist.radio.snapshot_timer", "22");
+}
+
+void gsm_properties(char const default_network[])
+{
+    property_set("ro.telephony.default_network", default_network);
+    property_set("telephony.lteOnGsmDevice", "1");
+    property_set("ro.ril.radio.svn", "1");
+}
+
+void vendor_load_properties()
+{
+    std::string platform;
+    std::string bootmid;
+    std::string device;
+
+    platform = property_get("ro.board.platform");
+    if (platform != ANDROID_TARGET)
+        return;
+
+    bootmid = property_get("ro.boot.mid");
+
+    if (bootmid == "0P6B90000") {
+        /* melsuhl */
+        common_properties();
+        gsm_properties("9");
+        property_override("ro.product.model", "HTC One_M8 Eye");
+        property_override("ro.build.fingerprint", "htc/melsuhl_htc_europe/htc_melsuhl:6.0.1/MMB29M/713780.1:user/release-keys");
+        property_override("ro.build.description", "5.07.401.1 CL713780 release-keys");
+        property_override("ro.product.device", "htc_melsuhl");
+        property_override("ro.build.product", "htc_melsuhl");
+    } else {
+        /* melsuhl */
+        common_properties();
+        gsm_properties("9");
+        property_override("ro.product.model", "HTC One_M8 Eye");
+        property_override("ro.build.fingerprint", "htc/melsuhl_htc_europe/htc_melsuhl:6.0.1/MMB29M/713780.1:user/release-keys");
+        property_override("ro.build.description", "5.07.401.1 CL713780 release-keys");
+        property_override("ro.product.device", "htc_melsuhl");
+        property_override("ro.build.product", "htc_melsuhl");
+    }
+
+    device = property_get("ro.product.device");
+    ERROR("Found bootmid %s setting build properties for %s device\n", bootmid.c_str(), device.c_str());
+}
